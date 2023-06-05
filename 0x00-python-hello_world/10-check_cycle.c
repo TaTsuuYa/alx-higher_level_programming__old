ee25cb7c0a6fb4b2e71ce81bcce4996a311a46fe
TaTsuuYa <taoufik02yahyaoui@gmail.com>
2023-06-05 14:30:46 -0700

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * check_cycle - cheks if a singly linked list is cyclic
+ * @list: pointer to head of a singly linked list
+ *
+ * Return: 0 if no cycle, 1 if cyclic
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *first = list, *current = list->next;
+
+	while (current)
+		if (current == first)
+			return (1);
+
+	return (0);
+}
+
