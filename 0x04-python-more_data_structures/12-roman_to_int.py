302ab7e0b6018105bf0901c7ed803a4e2d7d2e14
Taoufik Yahyaoui <TaTsuuYa>
2023-06-14 23:45:45 +0000

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -15,6 +15,17 @@ def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
         return 0
     n = 0
-    for i in roman_string:
-        n += rom.get(i, 0)
+    for i in range(len(roman_string)):
+        if roman_string[i] not in rom:
+            return 0
+        if i < len(roman_string) - 1:
+            if roman_string[i] < roman_string[i + 1]:
+                n += rom[roman_string[i + 1]] - rom[roman_string[i]]
+                i += 1
+            else:
+                n += rom[roman_string[i]]
     return n
+
+
+roman_number = "IIIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
