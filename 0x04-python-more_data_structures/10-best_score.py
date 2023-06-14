c0f96342512ecb1577a73a69265b82dda4a70ab2
Taoufik Yahyaoui <TaTsuuYa>
2023-06-14 20:09:50 +0000

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    key = None
     if not a_dictionary:
         return None
 
+    key = list(a_dictionary.keys())[0]
     score = list(a_dictionary.values())[0]
     for k, e in a_dictionary.items():
         if e > score:
