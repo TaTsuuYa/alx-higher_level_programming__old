93d527e5c01529f1733800f56739424ffd0d21c3
Taoufik Yahyaoui <TaTsuuYa>
2023-06-26 19:07:11 +0000

    0. Factorize all the things!

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=[], x=0):	
     n = 0
     try:
         while n < x:
@@ -12,14 +12,3 @@ def safe_print_list(my_list=[], x=0):
 
     print()
     return n
-
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
