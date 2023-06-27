135a79dace1ea9b7ed3ff89d8a232c93174fb136
Taoufik Yahyaoui <TaTsuuYa>
2023-06-27 20:42:29 +0000

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""2. Size validation"""
+
+
+class Square:
+    """This is a square"""
+
+    def __init__(self, size=0):
+        """initializing the square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+            
+    def area(self):
+        return self.__size * self.__size
