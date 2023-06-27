0043aca91e6bc9de3bc83d170fff21cd95da945e
Taoufik Yahyaoui <TaTsuuYa>
2023-06-27 20:34:44 +0000

    3. Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""2. Size validation"""
+
+
+class Square:
+    """This is a square"""
+
+    def __init__(self, size=0):
+        """initializing the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
