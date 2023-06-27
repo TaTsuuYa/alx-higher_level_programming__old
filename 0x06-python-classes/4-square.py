07835e5b5e8b0009a5c695a53385b4259276994e
Taoufik Yahyaoui <TaTsuuYa>
2023-06-27 20:43:01 +0000

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, size):
         if not isinstance(size, int):
@@ -20,6 +20,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-            
+
     def area(self):
         return self.__size * self.__size
