010f73bce091be6a92a580ca0a90184f78788d97
Taoufik Yahyaoui <TaTsuuYa>
2023-07-03 19:51:22 +0000

    6. How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -75,12 +75,3 @@ class Rectangle:
         """Bye, Bye"""
         print("Bye rectangle...")
         self.decrement_number_of_instances()
-
-
-my_rectangle_1 = Rectangle(2, 4)
-my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_1
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_2
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
