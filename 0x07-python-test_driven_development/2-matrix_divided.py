2a68ee65c84998fd47b3a1325bff911c63d36721
Taoufik Yahyaoui <TaTsuuYa>
2023-07-05 22:53:26 +0000

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -33,7 +33,7 @@ def matrix_divided(matrix, div):
                 "matrix must be a matrix (list of lists) of integers/floats")
         for n in lst:
             if not (isinstance(n, int) or isinstance(n, float)):
-                raise TypeError("matrix must be a matrix"
+                raise TypeError("matrix must be a matrix "
                                 "(list of lists) of integers/floats")
 
     for lst in range(len(matrix) - 1):
