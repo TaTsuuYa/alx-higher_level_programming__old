88b69b2f8f667eb2adc371f486379b71894840d8
TaTsuuYa <114253973+TaTsuuYa@users.noreply.github.com>
2023-07-05 08:25:18 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,11 +14,8 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print()
-        return
-
-    rec = ""
-    for y in range(size):
-        rec += ("#" * size) + ('\n' if y < size - 1 else '')
-    print(rec)
+    if size != 0:
+        rec = ""
+        for y in range(size):
+            rec += ("#" * size) + ('\n' if y < size - 1 else '')
+            print(rec)
