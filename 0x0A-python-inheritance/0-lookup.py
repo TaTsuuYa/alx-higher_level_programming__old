98c04555da7578c3f231e592d2ad83d69901d4df
Taoufik Yahyaoui <TaTsuuYa>
2023-07-10 19:33:15 +0000

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def lookup(obj: object):
+    """
+    Returns a list of object attributes and methods.
+    
+    Args:
+    	obj (object): the object which attributes gets returned
+    """
+    return list(obj.__dict__)
