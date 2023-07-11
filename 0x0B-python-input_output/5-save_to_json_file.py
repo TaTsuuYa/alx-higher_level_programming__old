14c9c44b4f23e905a996c4f0cb58d51e789f9b26
Taoufik Yahyaoui <TaTsuuYa>
2023-07-11 20:01:48 +0000

    5. Save Object to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""5. Save Object to a file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Saves an object to a file
+
+    Args:
+        my_obj (object): the saved object
+        filename (str)L name of the file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, filename)
