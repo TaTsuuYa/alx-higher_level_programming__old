25ca73c086c33b6a29c3e139fa1fec00b795aa7d
Taoufik Yahyaoui <TaTsuuYa>
2023-07-12 22:36:50 +0000

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,10 +7,34 @@ adds arguments to the lists
 and save it back to the json file
 """
 from sys import argv
+import json
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file')\
-    .load_from_json_file
+
+def save_to_json_file(my_obj, filename):
+    """
+    Saves an object to a file
+
+    Args:
+        my_obj (object): the saved object
+        filename (str)L name of the file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a string
+
+    Args:
+        filename (str): name of the file
+
+    Returns:
+        object
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
+
 
 filename = "add_item.json"
 
