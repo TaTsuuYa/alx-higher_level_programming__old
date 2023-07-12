1535c5cadd70b67755d9e9aa7b307b4a15778647
Taoufik Yahyaoui <TaTsuuYa>
2023-07-12 17:20:25 +0000

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""12. Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    pas = [[1] * x for x in range(1, n + 1)]
+    for i, lst in enumerate(pas):
+        for j, num in enumerate(lst):
+            try:
+                x = i - 1
+                y = j - 1
+                if x < 0 or y < 0:
+                    continue
+                pas[i][j] = pas[x][j] + pas[x][y]
+            except IndexError:
+                pass
+    return pas
+
+
+print(pascal_triangle(4))
