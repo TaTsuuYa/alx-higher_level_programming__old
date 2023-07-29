6d215e479fa1af15d66b5ed8cca651492a521d8d
TaTsuuYa <114253973+TaTsuuYa@users.noreply.github.com>
2023-07-29 10:52:37 +0000

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""0. Read file"""
-
-
-def read_file(filename=""):
-    """Prints a filename."""
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")
